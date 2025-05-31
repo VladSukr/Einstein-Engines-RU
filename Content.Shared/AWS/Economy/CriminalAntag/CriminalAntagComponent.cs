1417eed33e8e8d8041444453fec322763b21d33a
aw-c <shitikov_1910@mail.ru>
2025-06-01 00:02:16 +0300

    criminal antag init

---
+++ Content.Shared/AWS/Economy/CriminalAntag/CriminalAntagComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.AWS.CriminalAntag;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CriminalAntagComponent : Component
+{
+}
---
+++ Content.Shared/AWS/Economy/CriminalAntag/CriminalAntagRuleComponent.cs
@@ -0,0 +1,5 @@
+[RegisterComponent]
+public sealed partial class CriminalAntagRuleComponent : Component
+{
+    
+}
